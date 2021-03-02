73f3d5aa5c5960a8be3e298f95aa2bf0e789902a
Hacker9896 <sss12212001@gmail.com>
2021-03-02 09:35:32 +0530

    Edx

--- finding repeating letters in a word.c
+++ finding repeating letters in a word.c
@@ -31,24 +31,48 @@ Output:
 */
 
 #include <stdio.h>
-int main(){
-	char name[50];
-	int i, count;
-	int swap;
+int main(void)
+{
+    char word[51];
+    int length = 0;
+    int i, j;
+    char swap;
+    int repeats = 0;
+
+    scanf("%s", word);
 	
-	printf("Enter the name = ");
-	scanf("%s",name);
-	count = 0;
-	while (name != '\0'){
-		if(name[i] == name[i+1]){
-			count += 1;
-		}else if( name[i]<name[i+1]){
-			 swap = name[i];
-			 name[i] = name[i+1];
-			 name[i+1] = swap;
-		}
-		i++;
-			}
-	printf("the count is %d",count);
-	
+    while (word[length]!='\0')
+	length++;
+
+    //Sort the word by alphabetical order    
+    for(j=0;j<length-1; j++) {
+        for(i=0;i<length-1;i++) { 
+            if (word[i] > word[i+1]) {
+                swap = word[i];
+                word[i]   = word[i+1];
+                word[i+1] = swap;
+            }
+        }
+    }
+    
+    i = 0;
+    
+    //Check for repeating characters in the sorted word
+    while (i<length-1) {
+        if (word[i]==word[i+1]) {
+            repeats++;
+            j=i+2;
+
+            //Continues through the word until it reaches a new character
+            while (j<length && word[i]==word[j]) 
+                j++;
+            i = j;
+        } else {
+            i++;
+        }
+    }
+    
+    printf("%d", repeats);
+
+    return 0;
 }
